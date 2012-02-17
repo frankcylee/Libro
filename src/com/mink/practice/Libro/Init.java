9d2816e27cf48e697c3a99862ad2b1a90e0a27bf
Chen-Yi Lee <frank.cylee@gmail.com>
2012-02-17 23:07:49 +0800

    Initial checkin

---
+++ src/com/mink/practice/Libro/Init.java
@@ -0,0 +1,13 @@
+package com.mink.practice.Libro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Init extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
